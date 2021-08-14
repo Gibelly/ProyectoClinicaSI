fc404d91dcde159e8db9741e00f186d64745a14c
Gibelly <gibellytabora@gmail.com>
2021-08-14 17:36:06 -0600

    ajuste de vistas para Recepcion/citas

--- ProyectoClinica/Controllers/PatientController.cs
+++ ProyectoClinica/Controllers/PatientController.cs
@@ -54,7 +54,7 @@ namespace ProyectoClinica.Controllers
         {
             return View();
         }
-
+        
         // POST: Patient/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
