ec4b2355c3f7b837e9131f42be6d69662b6c6d82
mildredbarnica <mildredbarnica@gmail.com>
2021-08-14 23:13:23 -0600

    Modificacion de vistas login

--- ProyectoClinica/Models/Login.cs
+++ ProyectoClinica/Models/Login.cs
@@ -13,15 +13,19 @@ namespace ProyectoClinica.Models
         [Key]
         public int Id { get; set; }
 
+        [Display(Name = "Nombre")]
         public string Name { get; set; }
+        [Display(Name = "Apellidos")]
         public string Lastname { get; set; }
 
         [DisplayName("Nombre de Usuario")]
         public string User { get; set; }
 
+        [Display(Name = "Tipo de Usuario")]
         public int UserTypeId { get; set; }
 
         [Display(Name = "Tipo de Usuario")]
+
         [ForeignKey("UserTypeId")]
         public UserType Type { get; set; }
 
